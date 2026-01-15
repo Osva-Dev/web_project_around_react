183292501c6161263314f5a689b7b98e2e18ba3f
Osva-Dev <osvadev30@gmail.com>
2026-01-15 16:00:55 -0600

    add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,53 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="page">
-        <header className="header">
-          <img className="header__logo" src="" alt="Around the U.S. logo" />
-        </header>
-        <main className="main">
-          <section className="profile">
-            <div className="profile__user">
-              <div className="profile__picture-wrapper">
-                <img
-                  className="profile__picture"
-                  src=""
-                  alt="Profile Picture"
-                />
-              </div>
-              <div className="profile__information">
-                <div className="profile__custumise">
-                  <h1 className="profile__name">Osvaldo Ochoa</h1>
-                </div>
-                <p className="profile__profession">Web Developer JR</p>
-              </div>
+    <div className="page">
+      <header className="header">
+        <img
+          className="header__logo"
+          src="./images/content/logo.svg"
+          alt="Around the U.S."
+        />
+      </header>
+      <main className="main">
+        <section className="profile">
+          <div className="profile__user">
+            <div className="profile__picture-wrapper">
+              <img className="profile__picture" src="" alt="Profile Picture" />
+              <img
+                className="profile__icon"
+                src="./images/icons/edit-2.svg"
+                alt="Button Edit"
+              />
             </div>
-          </section>
-        </main>
-        <footer className="footer">
-          <p className="footer__copyright">&#169; 2024 Around The U.S.</p>
-        </footer>
-      </div>
-    </>
+            <div className="profile__information">
+              <div className="profile__custumise">
+                <h1 className="profile__name"></h1>
+                <button
+                  type="button"
+                  className="profile__button profile__button_edit"
+                >
+                  <img src="./images/icons/edit.png" alt="Edit Button" />
+                </button>
+              </div>
+              <p className="profile__profession"></p>
+            </div>
+          </div>
+          <div className="profile__add">
+            <button
+              type="button"
+              className="profile__button profile__button_add"
+            >
+              🞣
+            </button>
+          </div>
+        </section>
+        <section className="place"></section>
+      </main>
+      <footer className="footer">
+        <p className="footer__copyright">© 2024 Around The U.S.</p>
+      </footer>
+    </div>
   );
 }
 
